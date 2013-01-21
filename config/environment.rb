ca38b735cef62fbcae34bd1d461ad9aa2cf08c3f
OlivierRevial <olivier.revial@ingesup.com>
2013-01-21 23:27:20 +0100

    Merge avec le commit de Daniel + Cleaner

--- config/environment.rb
+++ config/environment.rb
@@ -4,4 +4,5 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 ProjetParis::Application.initialize!
 
-Scheduler.schedule
+Scheduler.schedule_clean
+Scheduler.schedule_parse
