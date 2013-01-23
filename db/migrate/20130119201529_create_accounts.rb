069287c27dfccea60efc37d5fd8a4e404b10990a
OlivierRevial <olivier.revial@ingesup.com>
2013-01-24 00:38:52 +0100

    Ajout d'un champ date dans la BDD pour ne pas parser le XML lorsqu'il n'a pas été mis à jour entre deux tours du Parser

--- db/migrate/20130119201529_create_accounts.rb
+++
@@ -1,9 +0,0 @@
-class CreateAccounts < ActiveRecord::Migration
-  def change
-    create_table :accounts do |t|
-      t.string :user_id
-
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20130120151828_add_credit_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddCreditToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :credit, :float, :null => false, :default => 40
-  end
-end
